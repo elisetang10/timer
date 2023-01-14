a35c79f04b6ff5138d2de18a0c2e40b3f2a544e6
elisetang10 <112234054+elisetang10@users.noreply.github.com>
2023-01-14 12:42:29 +0800

    Add files via upload

---
+++ timer.py
@@ -0,0 +1,12 @@
+import time
+seconds = input("enter the time in seconds: ")
+def countdown_timer(seconds):
+    while seconds>0:
+        mins = int(seconds/60)
+        secs = int(seconds%60)
+        timer = f"{mins}:{secs}"
+        print(timer, end="\r")
+        time.sleep(1)
+        seconds-= 1
+    print("times up!")
+countdown_timer(int(seconds))
